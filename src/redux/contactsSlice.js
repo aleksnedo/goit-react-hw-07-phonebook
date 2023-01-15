6a35985ca96da24fe81cc52664aa8f6d48d64ce7
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2023-01-15 22:32:48 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,43 +1,74 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const contactsInitialState = { items: [] };
+const contactsInitialState = [];
 
-const contactSlice = createSlice({
+const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: contactsInitialState,
+  initialState: {
+    contactsList: contactsInitialState,
+  },
   reducers: {
-    onContactAdd: {
-      reducer(state, action) {
-        state.items.push(action.payload);
-      },
-      prepare(contact) {
-        return {
-          payload: {
-            name: contact.name,
-            number: contact.number,
-            id: nanoid(),
-          },
-        };
-      },
+    onContactAdd(state, action) {
+      state.contactsList.push(action.payload);
     },
     onDeleteContact(state, action) {
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload
+      state.contactsList = state.contactsList.filter(
+        contact => contact.id !== action.payload
       );
-      state.items.splice(index, 1);
     },
   },
 });
+
 const persistConfig = {
-  key: 'contacts',
+  key: 'root',
   storage,
 };
 
-export const { onContactAdd, onDeleteContact } = contactSlice.actions;
-
 export const contactsReducer = persistReducer(
   persistConfig,
-  contactSlice.reducer
+  contactsSlice.reducer
 );
+
+export const { onContactAdd, onDeleteContact } = contactsSlice.actions;
+
+// const contactsInitialState = { items: [] };
+
+// const contactSlice = createSlice({
+//   name: 'contacts',
+//   initialState: contactsInitialState,
+//   reducers: {
+//     onContactAdd: {
+//       reducer(state, action) {
+//         state.items.push(action.payload);
+//       },
+//       prepare(contact) {
+//         return {
+//           payload: {
+//             name: contact.name,
+//             number: contact.number,
+//             id: nanoid(),
+//           },
+//         };
+//       },
+//     },
+//     onDeleteContact(state, action) {
+//       const index = state.items.findIndex(
+//         contact => contact.id === action.payload
+//       );
+//       state.items.splice(index, 1);
+//     },
+//   },
+// });
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
+
+// export const { onContactAdd, onDeleteContact } = contactSlice.actions;
+
+// export const contactsReducer = persistReducer(
+//   persistConfig,
+//   contactSlice.reducer
+// );
