029d9a9a647ae254872d7ca5058d654c19439fbc
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2023-01-16 22:36:47 +0200

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,60 +1,56 @@
-import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+// import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { onContactAdd } from 'redux/contactsSlice';
 import { getContacts } from 'redux/selectors';
 import { FormStyle, Label, Input, Button } from './Form.styled';
 
+const initialValues = {
+  name: '',
+  number: '',
+};
+
 export default function Form() {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-  const contacts = useSelector(getContacts);
+  // const [name, setName] = useState('');
+  // const [number, setNumber] = useState('');
+  const { contacts } = useSelector(getContacts);
   const dispatch = useDispatch();
 
-  const resetForm = () => {
-    setName('');
-    setNumber('');
-  };
+  const handleSubmit = (values, { resetForm }) => {
+    const { name, number } = values;
+    const contact = {
+      name,
+      number,
+    };
+    const dublicateContact = findDublicate(contact, contacts);
+    dublicateContact
+      ? alert(`${contact.name} or ${contact.number} is already in contacts`)
+      : dispatch(onContactAdd({ ...values, id: nanoid() }));
 
-  const handleChange = e => {
-    const { name, value } = e.currentTarget;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-      case 'number':
-        setNumber(value);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const contact = { name, number };
-    for (let item of contacts) {
-      if (contact.name.toLowerCase() === item.name.toLowerCase()) {
-        alert(`${contact.name} is already in contacts`);
-        resetForm();
-        return;
-      }
-    }
-    dispatch(onContactAdd(contact));
     resetForm();
   };
 
+  const findDublicate = (contact, contacts) => {
+    return contacts.find(
+      item =>
+        item.name.toLowerCase() === contact.name.toLowerCase() ||
+        item.number === contact.number
+    );
+  };
+
   return (
-    <FormStyle onSubmit={handleSubmit}>
+    <FormStyle onSubmit={handleSubmit} initialValues={initialValues}>
       <Label>
         Name
         <Input
           type="text"
           name="name"
-          value={name}
+          // value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={handleChange}
+          // onChange={handleChange}
         />
       </Label>
       <Label>
@@ -62,11 +58,11 @@ export default function Form() {
         <Input
           type="tel"
           name="number"
-          value={number}
+          // value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={handleChange}
+          // onChange={handleChange}
         />
       </Label>
 
